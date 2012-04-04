8c18125b0613377461336b2337664bbcda92b555
yt <demoonlit@panathenaia.halfmoon.jp>
2012-04-04 11:19:50 +0900

    Fix for 64bit environment

--- source/gmp.adb
+++ source/gmp.adb
@@ -17,17 +17,22 @@ package body GMP is
 	
 	procedure mpz_init_set_Long_Long_Integer (
 		rop : not null access C.gmp.mpz_struct;
-		op : in Long_Long_Integer) is
-	begin
-		if op in
+		op : in Long_Long_Integer)
+	is
+		subtype ui is Long_Long_Integer range
 			Long_Long_Integer (C.unsigned_long'First) ..
-			Long_Long_Integer (C.unsigned_long'Last)
-		then
-			C.gmp.mpz_init_set_ui (rop, C.unsigned_long (op));
-		elsif op in
+			Long_Long_Integer (
+				C.unsigned_long_long'Min (
+					C.unsigned_long_long (C.unsigned_long'Last),
+					C.unsigned_long_long (Long_Long_Integer'Last)));
+		subtype si is Long_Long_Integer range
 			Long_Long_Integer (C.signed_long'First) ..
-			Long_Long_Integer (C.signed_long'Last)
-		then
+			Long_Long_Integer (C.signed_long'Last);
+	begin
+		pragma Warnings (Off, "explicit membership test may be optimized away");
+		if op in ui then
+			C.gmp.mpz_init_set_ui (rop, C.unsigned_long (op));
+		elsif op in si then
 			C.gmp.mpz_init_set_si (rop, C.signed_long (op));
 		else
 			C.gmp.mpz_init_set_si (
@@ -44,6 +49,7 @@ package body GMP is
 				rop,
 				C.unsigned_long'Mod (op));
 		end if;
+		pragma Warnings (On, "explicit membership test may be optimized away");
 	end mpz_init_set_Long_Long_Integer;
 	
 end GMP;
